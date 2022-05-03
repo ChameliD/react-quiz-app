9fbc94ff2ff50e7beba64483b04043dbdaa1504d
Chameli Dilshani <chameli.d@eyepax.com>
2022-05-03 16:15:50 +0530

    Add Routers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Header from './components/Header/Header';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz';
+import Result from './Pages/Result/Result';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app" style={{backgroundImage:"url(./background2.png"}}>
         <Header/> 
+
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/quiz" element={<Quiz/>}/>
+          <Route exact path="/result" element={<Result/>}/>
+        </Routes>
       </div>
       <Footer/>
     </BrowserRouter>
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import "./Home.css"
+
+const Home=()=>{
+    return(
+        <div className="content">
+            <div>
+                <div className="setting">
+                    <span style={{fontSize:30}}>Quiz Setting</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
