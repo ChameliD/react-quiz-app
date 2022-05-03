b37d9934d143a25e3234f3f7de8f353a947446a6
Chameli Dilshani <chameli.d@eyepax.com>
2022-05-03 14:42:59 +0530

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import Header from './components/Header';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app" style={{backgroundImage:"url(./background2.png"}}> 
+    <BrowserRouter>
+      <div className="app" style={{backgroundImage:"url(./background2.png"}}>
+        <Header/> 
       </div>
+    </BrowserRouter>
   );
 }
 
